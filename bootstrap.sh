e6d03f5c43af6096c06fa9f7821219735669b0ba
Atsushi Nakajyo <jyotti@gmail.com>
2015-03-18 10:37:20 +0900

    rsyncでのセットアップをinclude優先にした

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,23 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-        rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-              --exclude "README.md" --exclude "LICENSE.txt" --exclude "*.swp" \
-              --exclude "Brewfile" --exclude "Caskfile" --exclude "*.bak" --exclude ".extra" \
-              --exclude "git/" --exclude "subtrees/" --exclude "brew.sh" \
-              -av --no-perms . ~
-        rsync -av --no-perms git/gitignore ~/.gitignore
-        rsync -av --no-perms git/gitconfig ~/.gitconfig
+  rsync --include ".bashrc" --include ".bash_profile" --include ".functions" --include ".path" \
+    --include ".bash_prompt" --include ".exports" --include ".aliases" --include ".extra" \
+    --include ".inputrc" --include ".osx" --include ".tmux.conf" \
+    --include ".vimrc" --include ".vim" \
+    --exclude "*" --exclude "*/" \
+    -av --no-perms . ~
 
-        source ~/.bash_profile
+  source ~/.bashrc
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-        doIt
+  doIt
 else
-        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-                doIt
-        fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
