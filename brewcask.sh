51687da51836c972a8f2fd372993b047692754d2
Atsushi Nakajyo <jyotti@gmail.com>
2016-03-12 17:56:07 +0900

    Homebrew Cask 使っているのだけに整理

--- brewcask.sh
+++ brewcask.sh
@@ -4,27 +4,12 @@ brew install caskroom/cask/brew-cask
 # alternate versions
 brew tap caskroom/versions
 
-# App
-brew cask install google-chrome
-brew cask install google-japanese-ime
-brew cask install iterm2
-brew cask install xtrafinder
-brew cask install bettertouchtool
-brew cask install github
-brew cask install virtualbox
-brew cask install mysqlworkbench
-
 # Development
+brew cask install java
 brew cask install java7
-brew cask install java6
 brew cask install vagrant
 
 # Quick Look plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
 brew cask install genymotion
-brew cask install android-studio
-
-brew cask install atom
-brew cask install mou
-brew cask install macvim-kaoriya
